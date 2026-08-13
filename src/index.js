ec5e91b72c7421782158135b0ed256cacfdc56a9
prince <princepatel27806@gmail.com>
2026-08-13 19:11:12 +0530

    feat: initialize Express app and set up middleware for JSON and URL-encoded data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import dotenv from "dotenv";
 import dns from "node:dns";
 import connectdb from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env"
+})
 
 
 dotenv.config();
 
+// Fix for Windows/ISP DNS resolution issues with MongoDB Atlas SRV records
+// dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 connectdb()
+
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(` Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+  });
